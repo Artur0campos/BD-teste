d92d2b41c27e0772f539d2be096009e6985e2077
Artur0campos <arturcamposcamara@gmail.com>
2025-05-30 11:05:48 -0300

    first commit

---
+++ assinante.py
@@ -0,0 +1,139 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def conectar_ao_banco():
+    try:
+        conexao = mysql.connector.connect(
+            host="127.0.0.1",
+            port=3306,
+            user="root",
+            database="filiais_BD"
+        )
+        if conexao.is_connected():
+            print("Conexão bem-sucedida ao banco de dados!")
+            return conexao
+
+    except Error as e:
+        print(f"Erro ao conectar ao banco de dados: {e}")
+        return None
+
+
+conn = conectar_ao_banco()
+
+if conn: 
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM assinante_tbl")
+    resultado = cursor.fetchall()
+
+    for linha in resultado:
+        print(linha)
+
+    cursor.close()
+    conn.close()
+
+def inserir_tipo(descricao):
+    conn = conectar_ao_banco()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO assinante_tbl (nome) VALUES (%s)", (descricao,))
+        conn.commit()
+        print("tipo inserido com sucesso")
+        conn.close()
+        cursor.close()
+
+def inserir_ramo(descricao):
+    conn = conectar_ao_banco()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO assinante_tbl (nome) VALUES (%s)", (descricao,))
+        conn.commit()
+        print("ramo inserido com sucesso")
+        conn.close()
+        cursor.close()
+
+def inserir_assinante(nome):
+    conn = conectar_ao_banco()
+    if conn:
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO assinante_tbl (nome) VALUES (%s)", (nome,))
+        conn.commit()
+        print("Assinante inserido com sucesso")
+        conn.close()
+        cursor.close()
+
+
+def listar_assinantes():
+    conn = conectar_ao_banco()
+    if conn:
+        cursor =  conn.cursor()
+        cursor.execute("SELECT * FROM assinante_tbl")
+        for cd_assinante,nome in cursor.fetchall():
+            print(f"CD_assinante: {cd_assinante} - Nome: {nome}")
+        cursor.close()
+        conn.close()
+
+
+def update_assinante(cd_assinante, novo_nome):
+    conn = conectar_ao_banco()
+    if conn:
+        cursor = conn.cursor()
+        sql = "UPDATE assinantes SET nome = %s WHERE id = %s"
+        cursor.execute(sql, (novo_nome, cd_assinante))
+        conn.commit()
+        cursor.close()
+        conn.close()
+
+def delete_assinante(cd_assinante):
+    conn = conectar_ao_banco()
+    if conn:
+        cursor = conn.cursor()
+        sql = "DELETE FROM assinantes WHERE id = %s"
+        cursor.execute(sql, (cd_assinante,))
+        conn.commit()
+        print("Assinante excluído com sucesso")
+        cursor.close()
+
+
+
+def menu():
+    while True:
+        print("\n--- MENU CRUD ---")
+        print("1 - Inserir assinante")
+        print("2 - Listar assinantes")
+        print("3 - Atualizar assinante")
+        print("4 - Deletar assinante")
+        print("0 - Sair")
+
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            nome = input("Nome do assinante: ")
+            descricao_ramo = input("Descrição do ramo: ")
+            descricao_tipo = input("Descrição do tipo: ")
+            inserir_ramo(descricao_ramo)
+            inserir_tipo(descricao_tipo)
+            inserir_assinante(nome)
+            print("Assinante inserido com sucesso!")
+
+        elif opcao == "2":
+            listar_assinantes()
+
+        elif opcao == "3":
+            id = int(input("ID do assinante a atualizar: "))
+            novo_nome = input("Novo nome: ")
+            update_assinante(id, novo_nome)
+
+        elif opcao == "4":
+            id = int(input("ID do assinante a deletar: "))
+            delete_assinante(id)
+
+        elif opcao == "0":
+            print("Saindo...")
+
+        else:
+            print("Opção inválida.")
+
+
+
+menu()
